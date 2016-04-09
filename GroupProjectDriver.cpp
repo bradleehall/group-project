aa18591def49356de98ebccc96d08e45bf1bd494
matoro <matoro@users.noreply.github.com>
2016-04-08 23:16:20 -0500

    uploaded project files
    These are what we'll be doing the actual work in.

---
+++ GroupProjectDriver.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : Tetris Driver.cpp
+// Author      : Dr. Booth
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Main Driver For Group Project
+//============================================================================
+
+#include <iostream>
+#include "GroupProject.h"
+using namespace std;
+
+void Player(void);
+GroupProject* app;
+
+int main() {
+	GLUT_Plotter* g = new GLUT_Plotter(); //Create plotter
+	app = new GroupProject(g);                 //Create Game (Tetris)
+	g->RegisterIdleFunc(Player);          //Register Callback
+	g->MainLoop();                        //Enter main loop
+	return 0;
+}
+
+void Player(void){
+	app->Play();
+}
