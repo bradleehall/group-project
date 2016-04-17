5c1b473b51c0f6050f971a63bec6b2abd651adc3
matoro <matoro@users.noreply.github.com>
2016-04-17 17:54:04 -0500

    filled out SplayNode functions & made templated

--- GroupProject.h
+++ GroupProject.h
@@ -2,7 +2,6 @@
 * GroupProject.h
 *
 *  Created on: Mar 28, 2014
-*  Created on: Mar 28, 2014
 *      Author: Dr. Booth
 */
 
@@ -14,46 +13,82 @@
 /*
 This is a basic implementation, might move it out of header,
 deconstructor must set pointers to null.
+
+No, keep splay tree in the header since it's templated.  --Michael
 */
-class SplayNode {
+template <class Base>
+class SplayNode
+{
 private:
-	int value;
+	Base value;
 	SplayNode *left, *right;
 public:
-	SplayNode();
-
-	~SplayNode() {
-		left = NULL;
-		right = NULL;
+	SplayNode()
+	{
+	    this->value = Base();
+        this->left = this->right = NULL;
 	}
-	SplayNode(int val)
+
+	~SplayNode()
+	{
+		if(this->left)
+        {
+            delete this->left;
+        }
+        if(this->right)
+        {
+            delete this->right;
+        }
+        this->left = this->right = NULL;
+	}
+
+	SplayNode(const Base &val)
 	{
 	    this->value = val;
 	    this->left = this->right = NULL;
 	}
-	SplayNode(int val, SplayNode* l, SplayNode* r)
+
+	SplayNode(const Base &val, SplayNode *l, SplayNode *r)
 	{
         this->value = val;
         this->left = l;
         this->right = r;
 	}
 
-	SplayNode* getRight() {
-		return right;
+	SplayNode(SplayNode *l, SplayNode *r)
+	{
+	    this->value = Base();
+	    this->left = l;
+	    this->right = r;
 	}
-	SplayNode* getLeft() {
-		return left;
+
+	SplayNode* getRight()
+	{
+		return this->right;
 	}
-	void setVal(int val) {
-		value = val;
+
+	SplayNode* getLeft()
+	{
+		return this->left;
+	}
+
+	void setVal(const Base &val)
+	{
+		this->value = val;
 		return;
+	}
+
+	const Base getVal()
+	{
+	    return this->value;
 	}
 };
 
-
-class SplayTree {
+template <class Base>
+class SplayTree
+{
 private:
-	SplayNode* root;
+	SplayNode<Base>* root;
 public:
 	//right rotation, used with "locate value", really complicated
 	//so here's some pseudocode, right rotation is basically the same
@@ -67,15 +102,28 @@ public:
 	set temp->right to temp ->left
 	set temp->left to temp2
 	*/
-	void RRotation(SplayNode* top);
-	void LRotation(SplayNode* top);
+	SplayNode<Base>* RRotation(SplayNode<Base> *top)
+	{
+	    SplayNode<Base> *temp = top->getLeft();
+	    top->left = temp->getRight();
+	    temp->right = top;
+	    return temp;
+	}
+
+	SplayNode<Base>* LRotation(SplayNode<Base> *top)
+	{
+        SplayNode<Base> *temp = top->getRight();
+        top->right = temp->getLeft();
+        temp->left = top;
+        return temp;
+	}
 
 	//you may or may not realize that double rotations are easy if you use
 	//two single rotation function calls
-	void RRRotation(SplayNode* top);
-	void RLRotation(SplayNode* top);
-	void LRRotation(SplayNode* top);
-	void LLRotation(SplayNode* top);
+	//void RRRotation(SplayNode<Base> *top);
+	//void RLRotation(SplayNode<Base> *top);
+	//void LRRotation(SplayNode<Base> *top);
+	//void LLRotation(SplayNode<Base> *top);
 
 	//return 0 if not found, 1 if found,
 	//make ptr the address of the pointer you're using in the function call.
@@ -83,7 +131,8 @@ public:
 	//point to the last valid node that isn't NULL
 	//This can be done iteratively
 	//it should also call "movetotop" (see below)
-	bool FindValue(SplayNode **ptr);
+
+	//bool FindValue(SplayNode<Base> **ptr);
 
 	//everything hard we learned in class
 	//this function recursively moves the value at ptr to target.
@@ -91,16 +140,122 @@ public:
 	//target(at the place where ptr would be if it was in range of a
 	//double rotation, keep in mind our rotates don't change the
 	//address location of parents so it should be fine recursively)
-	void MoveToTop(SplayNode *ptr, SplayNode* target);
+
+	//void MoveToTop(SplayNode<Base> *ptr, SplayNode<Base> *target);
+
+	SplayNode<Base>* splay(SplayNode<Base> *root, const Base &splayval)
+	{
+        if((!root) || (root->getVal() == splayval))
+        {
+            return root;
+        }
+        if(root->getVal() > splayval)
+        {
+            if(!(root->getLeft()))
+            {
+                return root;
+            }
+            if(root->getLeft->getVal() > splayval)
+            {
+                root->left->left = splay(root->getLeft()->getLeft(),
+                                          splayval);
+                root = RRotation(root);
+            }
+            else if(root->getLeft()->getVal() < splayval)
+            {
+                root->left->right = splay(root->getLeft()->getRight(),
+                                           splayval);
+                if(root->getLeft()->getRight())
+                {
+                    root->left = LRotation(root->getLeft());
+                }
+            }
+            return((!(root->getLeft()))? root: RRotation(root));
+        }
+        else
+        {
+            if(!(root->getRight()))
+            {
+                return root;
+            }
+            if(root->getRight->getVal() > splayval)
+            {
+                root->right->left = splay(root->getRight()->getLeft(),
+                                           splayval);
+                if(root->getRight()->getLeft())
+                {
+                    root->right = RRotation(root->getRight());
+                }
+                else if(root->getRight()->getVal() < splayval)
+                {
+                    root->right->right = splay(root->getRight()->getRight(),
+                                                splayval);
+                    root = LRotation(root);
+                }
+                return((!(root->getRight()))? root: LRotation(root));
+            }
+        }
+	}
 
 
 	//basically a combination of findValue and movetotop with a short part where
 	//you use the "new" operand
-	void AddValue();
-	void RemoveValue();
+	void AddValue(SplayNode<Base> *root, const Base &newval)
+	{
+        if(!root)
+        {
+            root = new SplayNode<Base>(newval);
+        }
+        root = splay(root, newval);
+        if(root->getVal() == newval)
+        {
+            return;
+        }
+        SplayNode<Base> *temp = new SplayNode<Base>(newval);
+        if(root->getVal() > newval)
+        {
+            temp->right = root;
+            temp->left = root->getLeft();
+            root->left = NULL;
+        }
+        else
+        {
+            temp->left = root;
+            temp->right = root->getRight();
+            root->right = NULL;
+        }
+        return temp;
+	}
 
-	//as of now I don't see anything else to add here except maybe a printpreorder
-	//but if it's not required, this is where I'd like us to focus
+	void RemoveValue(const Base &delval)
+	{
+	    if(!(root))
+        {
+            return;
+        }
+        this->root = splay(this->root, delval);
+        if(delval == root->getVal())
+        {
+            if(!(this->root->getLeft()))
+            {
+                this->root = this->root->getRight();
+            }
+            else
+            {
+                SplayNode<Base> *temp = this->root->getRight();
+                this->root = this->root->getLeft();
+                splay(this->root, delval);
+                this->root->getRight() = temp;
+            }
+        }
+        else
+        {
+            cerr << "[error] Value " << delval << " not found" << endl;
+        }
+	}
+
+	//as of now I don't see anything else to add here except maybe a
+	//printpreorder but if it's not required, this is where I'd like us to focus
 };
 
 
