9b6d1d6a60a543f630fa33a6ebd47aa6b55493c4
matoro <matoro@users.noreply.github.com>
2016-04-18 23:35:25 -0500

    some experimental stuff and fixes

--- GroupProject.cpp
+++ GroupProject.cpp
@@ -7,6 +7,8 @@
  */
 
 #include "GroupProject.h"
+#include <cmath>
+#include <cstdlib>
 
 //GroupProject Constructor
 
@@ -14,7 +16,7 @@ GroupProject::GroupProject(GLUT_Plotter* g){
 	this->g = g;
 }
 
-GroupProject::GroupProject()
+GroupProject::~GroupProject()
 {
 	//end game;
 	delete g;
@@ -73,5 +75,116 @@ void GroupProject::Play(void){
 	}
 
 	// Update screen - draw game
-	g->Draw();
+	g->display();
+}
+
+//everything from here on is WIP--let Michael know if you edit it
+//most of it is directly copied.  it will need to be adapted to
+//more suit the group style, format, etc.
+void drawCircle(float segments, float radius, float sx, float sy)
+{
+    float theta = 2 * 3.1415926 / segments;
+    float tan_factor = tanf(theta);
+    float radial_factor = cosf(theta);
+    float x = radius;
+    float y = 0;
+    int cache_pt = 0;
+    double cache_x;
+    double cache_y;
+    glBegin(GL_LINES);
+    for (int ii = 0; ii < segments; ii++)
+    {
+        if(!cache_pt)
+        {
+            cache_x = x+sx;
+            cache_y = y+sy;
+            cache_pt = 1;
+        }
+        else
+        {
+            //glVertex2f(cache_x,cache_y);
+            glVertex2f(x + sx, y + sy);
+            cache_x = x+sx;
+            cache_y = y+sy;
+        }
+        float tx = -y;
+        float ty = x;
+        x += tx * tan_factor;
+        y += ty * tan_factor;
+        x *= radial_factor;
+        y *= radial_factor;
+    }
+    glEnd();
+}
+
+void draw_line(float x1, float y1, float x2, float y2)
+{
+    glBegin(GL_LINES);
+    glVertex2f(x1,y1);
+    glVertex2f(x2,y2);
+    glEnd();
+}
+
+void draw_text(char* text, float x, float y)
+{
+    GLvoid *font_style = GLUT_BITMAP_TIMES_ROMAN_24;
+    glRasterPos3f(x, y, 0);
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        glutBitmapCharacter(font_style, text[i]);
+    }
+}
+
+void drawNode(SplayNode<int> *t_root, float x1, float y1, int level)
+{
+    if (!(t_root))
+    {
+        return;
+    }
+    float segments = 25;
+    float radius = 1.0;
+    float left_angle = 245;
+    float right_angle = 115;
+    float branch_length = 12 - level*2.5;
+    float angle_change = 20;
+
+    drawCircle(segments, radius, x1, y1);
+
+    char buff[5];
+    itoa(t_root->getVal(), buff, 10);
+    draw_text(buff, x1, y1);
+
+    if(t_root->getLeft())
+    {
+        float angle = left_angle - level*angle_change;
+        double radian = angle*3.14/180;
+        float m = (double)tan((double)radian);
+        float x2 = x1 + branch_length * sin((double) radian);
+        float y2 = y1 + branch_length * cos((double) radian);
+        drawNode(t_root->getLeft(), x2, y2, level+1);
+        draw_line(x1, y1, x2, y2);
+    }
+    if(t_root->getRight())
+    {
+        float angle = right_angle + level*angle_change;
+        float radian = angle*3.14/180;
+        float m = (double)tan((double)radian);
+        float x2 = x1 + branch_length * sin((double) radian);
+        float y2 = y1 + branch_length * cos((double) radian);
+        drawNode(t_root->getRight(), x2, y2, level+1);
+        draw_line(x1, y1, x2, y2);
+    }
+}
+
+void display()
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    glTranslatef(0, 10, -30);
+    glColor3f(1, 1, 1);
+
+    drawNode(root, 0, 0, 0);
+
+    glutSwapBuffers();
 }
